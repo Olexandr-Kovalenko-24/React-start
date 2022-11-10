93fd0a97dbcb63e24697ad0ad11443e43fe8e2c2
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-10 14:15:34 +0200

    props

---
+++ src/Greeting/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Greeting extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <h2>Hello, {this.props.userName}</h2>
+            </div>
+        )
+    }
+}
