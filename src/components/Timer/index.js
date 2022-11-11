4d8a69abb908d1bf70d84135a03a6f6be23289d3
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-11 22:58:29 +0200

    Add changing button text task res v2

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -6,7 +6,8 @@ class Timer extends Component {
         super(props);
         this.state = {
             time: new Date(0,0,0,0,0,0),
-            isPaused: false
+            isPaused: false,
+            buttonStage: 'Start'
         }
         this.intervalId = null;
     }
@@ -14,7 +15,8 @@ class Timer extends Component {
     start = () => {
         this.intervalId = setInterval(() => {
             const {time} = this.state;
-            const newDate = addSeconds(time, 1)
+            const newDate = addSeconds(time, 1);
+            // this.buttonStage = 'Start';
             this.setState({
                 time: newDate,
             })
@@ -26,21 +28,29 @@ class Timer extends Component {
     }
 
     pause = () => {
-        clearInterval(this.intervalId)
+        clearInterval(this.intervalId);
+        this.setState({
+            buttonStage: 'Continue'
+        })
     }
 
     click = () => {
         if(this.intervalId){
             this.pause();
             this.intervalId = !this.intervalId;
-        } else {this.start()}
+        } else {
+            this.setState({
+                buttonStage: 'Pause'
+            })
+            this.start()}
     }
 
     clear = () => {
         clearInterval(this.intervalId);
         this.intervalId = null;
         this.setState({
-            time: new Date(0,0,0,0,0,0)
+            time: new Date(0,0,0,0,0,0),
+            buttonStage: 'Start'
         })
     }
 
@@ -49,7 +59,7 @@ class Timer extends Component {
         return (
             <div>
                 <h1>{format(time, 'HH:mm:ss')}</h1>
-                <button onClick={this.click}>Start</button>
+                <button onClick={this.click}>{this.state.buttonStage}</button>
                 <button onClick={this.clear}>Clear</button>
             </div>
         );
