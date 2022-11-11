b3bef3460706746dd55fdb4f28ad4f77bbff115e
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-11 23:17:28 +0200

    Add styles hometask timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import {format, addSeconds} from 'date-fns';
+import { format, addSeconds } from 'date-fns';
+import './style.css'
 
 class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: new Date(0,0,0,0,0,0),
+            time: new Date(0, 0, 0, 0, 0, 0),
             isPaused: false,
             buttonStage: 'Start'
         }
@@ -14,7 +15,7 @@ class Timer extends Component {
 
     start = () => {
         this.intervalId = setInterval(() => {
-            const {time} = this.state;
+            const { time } = this.state;
             const newDate = addSeconds(time, 1);
             // this.buttonStage = 'Start';
             this.setState({
@@ -23,8 +24,8 @@ class Timer extends Component {
         }, 1000)
     }
 
-    componentWillUnmount(){
-    this.clear();
+    componentWillUnmount() {
+        this.clear();
     }
 
     pause = () => {
@@ -35,32 +36,35 @@ class Timer extends Component {
     }
 
     click = () => {
-        if(this.intervalId){
+        if (this.intervalId) {
             this.pause();
             this.intervalId = !this.intervalId;
         } else {
             this.setState({
                 buttonStage: 'Pause'
             })
-            this.start()}
+            this.start()
+        }
     }
 
     clear = () => {
         clearInterval(this.intervalId);
         this.intervalId = null;
         this.setState({
-            time: new Date(0,0,0,0,0,0),
+            time: new Date(0, 0, 0, 0, 0, 0),
             buttonStage: 'Start'
         })
     }
 
     render() {
-        const {time} = this.state;
+        const { time } = this.state;
         return (
-            <div>
-                <h1>{format(time, 'HH:mm:ss')}</h1>
-                <button onClick={this.click}>{this.state.buttonStage}</button>
-                <button onClick={this.clear}>Clear</button>
+            <div className='wrapper'>
+                <h1 className="clock">{format(time, 'HH:mm:ss')}</h1>
+                <div>
+                    <button onClick={this.click} className="start-button button">{this.state.buttonStage}</button>
+                    <button onClick={this.clear} className="clear-button button">Clear</button>
+                </div>
             </div>
         );
     }
