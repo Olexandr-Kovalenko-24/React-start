6860d0bd45ec21c073fb109f2faa411e8401e152
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-11 21:02:51 +0200

    Timer hometask resolve

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -5,7 +5,8 @@ class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            time: new Date(0,0,0,0,0,0)
+            time: new Date(0,0,0,0,0,0),
+            isPaused: false
         }
         this.intervalId = null;
     }
@@ -15,31 +16,40 @@ class Timer extends Component {
             const {time} = this.state;
             const newDate = addSeconds(time, 1)
             this.setState({
-                time: newDate
+                time: newDate,
             })
         }, 1000)
-
-    }
-    
-    componentDidMount(){
-        // this.start();
     }
 
     componentWillUnmount(){
-        this.stop();
+    this.clear();
     }
 
-    stop = () => {
+    pause = () => {
         clearInterval(this.intervalId)
+    }
+
+    click = () => {
+        if(this.intervalId){
+            this.pause();
+            this.intervalId = !this.intervalId;
+        } else {this.start()}
+    }
+
+    clear = () => {
+        clearInterval(this.intervalId);
+        this.setState({
+            time: new Date(0,0,0,0,0,0)
+        })
     }
 
     render() {
         const {time} = this.state;
         return (
             <div>
-                <h1>{format(time, 'hh:mm:ss')}</h1>
-                <button onClick={this.start}>Go</button>
-                <button onClick={this.stop}>Stop</button>
+                <h1>{format(time, 'HH:mm:ss')}</h1>
+                <button onClick={this.click}>Start</button>
+                <button onClick={this.clear}>Clear</button>
             </div>
         );
     }
