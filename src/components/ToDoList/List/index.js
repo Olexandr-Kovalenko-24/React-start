2b3ea0010cf7801538599b16f7ca9124f8ce059a
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-16 14:43:00 +0200

    Part 1 todoList

--- src/components/ToDoList/List/index.js
+++ src/components/ToDoList/List/index.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
 import ListElement from '../ListElement';
 
-// const arrayTask = [
-//     {
-//         'text': 'Read book',
-//         id: 1
-// },
-// {
-//     'text': 'Write song',
-//     id: 2
-// }
-// ]
-
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
             task: '',
+            taskList: []
         }
-        this.taskList = []
     }
 
     changeHandler = ({target: {name, value}}) => {
         this.setState({
             [name]: value,
         })
-        this.taskList.unshift(value);
     }
 
     submitHandler = (event) => {
         event.preventDefault();
-    }
-
-    addTask = () => this.taskList.map((taskList)=><ListElement text={taskList} key={new Date().getTime()}/>);
-
-    clickHandler = () => {
+        const {taskList, task} = this.state;
+        const todoObject = {
+            body: task,
+            id: taskList.length
+        }
         this.setState({
+            taskList: [...taskList, todoObject],
             task: ''
         })
-        if (this.taskList.length>1){
-            this.taskList.shift();
-        }
     }
 
+    addTask = () => {
+        const {taskList} = this.state;
+        return taskList.map(elem=><ListElement text={elem.body} key={elem.id}/>);
+    }
     render() {  
         return (
             <>
@@ -53,10 +43,10 @@ class List extends Component {
               value={this.state.task} 
               name='task'
               onChange={this.changeHandler} />
-              <button onClick={this.clickHandler}>Add to list</button>
+              <button>Add to list</button>
             </form>
             <ul>
-            <div>{this.addTask()}</div>
+            {this.addTask()}
             </ul>
             </>
         );
--- src/components/ToDoList/ListElement/index.js
+++ src/components/ToDoList/ListElement/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ListElement(props) {
     return (
-        <li>{props.text}</li>
+        <li>{props.text} <button>X</button></li>
     );
 }
 
