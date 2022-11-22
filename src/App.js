b3f810140375b5bd5058f62bf0cef8b8f17864aa
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-22 21:59:06 +0200

    react-router-dom menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
-import WindowResizer from "./components/WindowResizer";
-// import Card from "./components/UserCard";
-
-// import UserDashboard from './components/UserDashbord';
-// import ProductCards from "./components/ProductCards";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import ToDolist from './components/ToDoList'
+import LoginForm from './components/LoginForm'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
-            <WindowResizer />
+            <BrowserRouter>
+            <ul>
+                <li><Link to='/'>Go to Home</Link></li>
+                <li><Link to='/login'>Go to LoginForm</Link></li>
+                <li><Link to='/todo'>Go to ToDo</Link></li>
+            </ul>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/login" element={<LoginForm />}/>
+                    <Route path="/todo" element={<ToDolist />}/>
+                </Routes>
+            </BrowserRouter>
         )
     }
 }
 
 export default App;
+
+const Home = () => {
+    return <h1>You are home now!</h1>
+}
