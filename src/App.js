c1c17244150acc524c3be08eb8e5adaf2a3e3df0
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-05 16:35:15 +0200

    WindowSizer on hooks task resolve

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import MouseTracker from "./components/MouseTracker";
+import WindowSizer from "./components/WindowSizer";
+// import MouseTracker from "./components/MouseTracker";
+
 
 
 function App(props){
     const [isVisible, setVisible] = useState(true);
     return (
-        <>{isVisible && <MouseTracker />}</>
+        <>{isVisible && <WindowSizer />}</>
     )
 }
 
--- src/components/WindowSizer/index.js
+++ src/components/WindowSizer/index.js
@@ -1,32 +1,63 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class WindowSizer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            y: window.innerHeight,
-            x: window.innerWidth
+const WindowSizer = () => {
+    const [coordinates, setCoordinates] = useState({
+        x: window.innerWidth,
+        y: window.innerHeight
+    });
+
+    useEffect(() => {
+        window.addEventListener('resize', resizeHandler);
+        return () => {
+            window.removeEventListener('resize', resizeHandler)
         }
-    }
+    }, [])
 
-    resizeHandler = () => {
-        this.setState({
+    const resizeHandler = () => {
+        setCoordinates({
             y: window.innerHeight,
             x: window.innerWidth
         })
     }
 
-    componentDidMount () {
-        window.addEventListener('resize', this.resizeHandler)
-    }
-
-    componentWillUnmount () {
-        window.removeEventListener('resize', this.resizeHandler)
-    }
-
-    render() {
-        return this.props.children(this.state);
-    }
+    return (
+        <div>
+            <p>Current width: {coordinates.x}px</p>
+            <p>Current height: {coordinates.y}px</p>
+        </div>
+    );
 }
 
 export default WindowSizer;
+
+
+// class WindowSizer extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             y: window.innerHeight,
+//             x: window.innerWidth
+//         }
+//     }
+
+//     resizeHandler = () => {
+//         this.setState({
+//             y: window.innerHeight,
+//             x: window.innerWidth
+//         })
+//     }
+
+//     componentDidMount () {
+//         window.addEventListener('resize', this.resizeHandler)
+//     }
+
+//     componentWillUnmount () {
+//         window.removeEventListener('resize', this.resizeHandler)
+//     }
+
+//     render() {
+//         return this.props.children(this.state);
+//     }
+// }
+
+
