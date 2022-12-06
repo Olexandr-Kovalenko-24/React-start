921533010dad7897b8745bf650a1c1b448aa5513
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-06 14:14:57 +0200

    callback setState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import MouseTracker from "./components/MouseTracker";
-import FormsSquadPage from './pages/FormsSquadPage'
 
 
 
 function App(props){
     const [isVisible, setVisible] = useState(true);
     return (
-        <>{isVisible && <FormsSquadPage />}</>
+        <>{isVisible && <MouseTracker />}</>
     )
 }
 
