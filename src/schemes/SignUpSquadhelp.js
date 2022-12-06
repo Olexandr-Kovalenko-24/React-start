4e22b93b6b744972301b9106ca925372066a2ad3
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-06 10:10:01 +0200

    password===passwordConfirm yup SCHEMA

--- src/schemes/SignUpSquadhelp.js
+++ src/schemes/SignUpSquadhelp.js
@@ -5,12 +5,11 @@ export const SCHEMA = yup.object({
     lastName: yup.string().required('Field cannot be empty').min(1).max(50),
     displayName: yup.string().required('Display name should be more than 4 characters').min(1).max(50),
     email: yup.string().required().min(1).max(50).email('Please check the format of email address'),
-    password: yup.string().required().min(8).max(200).
-    matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 
+    password: yup.string().required().min(8).max(200)
+    .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 
     'Pass must contains at least one big and small letter, number and symbol'),
-    passwordConfirm: yup.string().required().min(8).max(200).
-    matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 
-    'Password confirmation needs to match original password'),
+    passwordConfirm: yup.string().required().min(8).max(200)
+    .oneOf([yup.ref('password'), null], 'Password confirmation needs to match original password'),
     joinAs: yup.string().required('Please specify whether you are a Creative or a Buyer'),
     allow: yup.boolean()
 })
