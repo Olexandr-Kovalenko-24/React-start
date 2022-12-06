c51353257bfde6675fc8577ce63ca93bc6d6e081
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-06 16:08:01 +0200

    Timer on hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import MouseTracker from "./components/MouseTracker";
-
+import Timer from './components/Timer';
+import UserDashboard from './components/UserDashbord'
 
 
 function App(props){
     const [isVisible, setVisible] = useState(true);
     return (
-        <>{isVisible && <MouseTracker />}</>
-    )
+        // <>{isVisible && <MouseTracker />}</>
+        <Timer />
+        )
 }
 
 export default App;
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,73 +1,109 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { format, addSeconds } from 'date-fns';
 import './style.css'
 
-class Timer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            time: new Date(0, 0, 0, 0, 0, 0),
-            isPaused: false,
-            buttonStage: 'Start'
+function Timer (props){
+    const [time, setTime] = useState(new Date(0,0,0,0,0,0));
+    const [isRunning, setRunning] = useState(false);
+
+    useEffect(() => {
+        let intervalId = null;
+        if(isRunning) {
+            intervalId = setTimeout(()=> {
+                setTime(addSeconds(time, 1))
+            }, 1000);
         }
-        this.intervalId = null;
-    }
-
-    start = () => {
-        this.intervalId = setInterval(() => {
-            const { time } = this.state;
-            const newDate = addSeconds(time, 1);
-            // this.buttonStage = 'Start';
-            this.setState({
-                time: newDate,
-            })
-        }, 1000)
-    }
-
-    componentWillUnmount() {
-        this.clear();
-    }
-
-    pause = () => {
-        clearInterval(this.intervalId);
-        this.setState({
-            buttonStage: 'Continue'
-        })
-    }
-
-    click = () => {
-        if (this.intervalId) {
-            this.pause();
-            this.intervalId = !this.intervalId;
-        } else {
-            this.setState({
-                buttonStage: 'Pause'
-            })
-            this.start()
+        return () => {
+            clearTimeout(intervalId)
         }
+    });
+
+
+    const reset = () => {
+        setTime(new Date(0,0,0,0,0,0))
     }
 
-    clear = () => {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-        this.setState({
-            time: new Date(0, 0, 0, 0, 0, 0),
-            buttonStage: 'Start'
-        })
+    const switchRunning = () => {
+        setRunning(!isRunning);
     }
 
-    render() {
-        const { time } = this.state;
+        const buttonText =  isRunning ? 'Stop' : 'Start';
+        const buttonClassname = isRunning ? 'green-btn' : 'red-btn';
         return (
-            <div className='wrapper'>
-                <h1 className="clock">{format(time, 'HH:mm:ss')}</h1>
-                <div>
-                    <button onClick={this.click} className="start-button button">{this.state.buttonStage}</button>
-                    <button onClick={this.clear} className="clear-button button">Clear</button>
-                </div>
+            <div>
+               <h1>{format(time, 'HH:mm:ss')}</h1> 
+               <button onClick={switchRunning} className={buttonClassname}>{buttonText}</button>
+               <button onClick={reset}>Clear</button>
             </div>
         );
     }
-}
 
 export default Timer;
+
+// class Timer extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             time: new Date(0, 0, 0, 0, 0, 0),
+//             isPaused: false,
+//             buttonStage: 'Start'
+//         }
+//         this.intervalId = null;
+//     }
+
+//     start = () => {
+//         this.intervalId = setInterval(() => {
+//             const { time } = this.state;
+//             const newDate = addSeconds(time, 1);
+//             // this.buttonStage = 'Start';
+//             this.setState({
+//                 time: newDate,
+//             })
+//         }, 1000)
+//     }
+
+//     componentWillUnmount() {
+//         this.clear();
+//     }
+
+//     pause = () => {
+//         clearInterval(this.intervalId);
+//         this.setState({
+//             buttonStage: 'Continue'
+//         })
+//     }
+
+//     click = () => {
+//         if (this.intervalId) {
+//             this.pause();
+//             this.intervalId = !this.intervalId;
+//         } else {
+//             this.setState({
+//                 buttonStage: 'Pause'
+//             })
+//             this.start()
+//         }
+//     }
+
+//     clear = () => {
+//         clearInterval(this.intervalId);
+//         this.intervalId = null;
+//         this.setState({
+//             time: new Date(0, 0, 0, 0, 0, 0),
+//             buttonStage: 'Start'
+//         })
+//     }
+
+//     render() {
+//         const { time } = this.state;
+//         return (
+//             <div className='wrapper'>
+//                 <h1 className="clock">{format(time, 'HH:mm:ss')}</h1>
+//                 <div>
+//                     <button onClick={this.click} className="start-button button">{this.state.buttonStage}</button>
+//                     <button onClick={this.clear} className="clear-button button">Clear</button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
